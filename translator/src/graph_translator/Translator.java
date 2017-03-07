279ad492c966471c269f91df753ee563b84a7e0d
Oskar Jedvert <oskar.jedvert@gmail.com>
2017-03-07 17:59:13 +0100

    add translator module

---
+++ translator/src/graph_translator/Translator.java
@@ -0,0 +1,18 @@
+package graph_translator;
+
+import graph_translator.model.NodeGrid;
+import model.Pattern;
+
+/**
+ * Created by vilddjur on 3/6/17.
+ */
+public class Translator {
+    /**
+     *
+     * @param graph
+     * @return
+     */
+    public static NodeGrid placeGraphOnGrid(Pattern graph){
+        return null;
+    }
+}
