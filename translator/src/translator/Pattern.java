07fffc79261806867c24a7fba5112528c3cc24d4
David Ström <dawid.strom@gmail.com>
2017-03-08 23:18:38 +0100

    Added a simple graph model for testing.

---
+++ translator/src/translator/Pattern.java
@@ -0,0 +1,11 @@
+package translator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by time on 3/8/17.
+ */
+public class Pattern {
+    public List<Node> pattern = null;
+}
